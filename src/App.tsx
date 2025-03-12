f97d0b9297ba54e4825c1de89506c38a7fa16a5f
pritshSV <swimngo.dev@gmail.com>
2025-03-12 16:05:14 +0530

    v7

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {HeroUIProvider} from "@heroui/react";
-import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router";
 import Landing from "./pages/landing";
 
 function App() {
@@ -19,7 +19,16 @@ function App() {
               { path: "/", element: <Landing/> }
           ]
       }
-  ])
+  ],{
+      future: {
+          v7_relativeSplatPath: true,
+          v7_startTransition: true,
+          v7_fetcherPersist: true,
+          v7_normalizeFormMethod: true,
+          v7_partialHydration: true,
+          v7_skipActionErrorRevalidation: true,
+      }
+  })
 
   return (
     <>
